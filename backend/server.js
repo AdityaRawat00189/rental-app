5f4900decd7f5c8faf382a29b7db0bd0c0fe63dd
rawataditya415@gmail.com <Escape>
2026-04-05 01:37:38 +0530

    Added Script in Package json

--- backend/server.js
+++ backend/server.js
@@ -38,6 +38,6 @@ app.use('/api/auth',authRoutes);
 app.use('/api/item',protect,itemRoutes);
 app.use('/api/booking',protect,bookingRoutes);
 
-app.listen(3000, ()=>{
-    console.log('🚀 Server is running on port 3000');
+app.listen(process.env.PORT || 3000, ()=>{
+    console.log('🚀 Server is running on port ' + (process.env.PORT || 3000));
 })
