e1eb7dbb58e46f9f81a4175f292f6da3c71aaba7
Aditya Rawat <rawataditya415@gmail.com>
2026-04-08 01:04:14 +0530

    Changed Transporter from Gmail to smtp

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -3,17 +3,33 @@ const nodemailer = require('nodemailer');
 
 const sendEmail = async (options) => {
     console.log('Attempting to send email to:', options.email);
+    // const transporter = nodemailer.createTransport({
+    //     service: 'gmail', // For Gmail in production
+        
+    //     // host: "smtp.mailtrap.io",   // Mailtrap SMTP host
+    //     // port: 2525, // For Mailtrap
+        
+    //     auth: {
+    //         user: process.env.EMAIL_USER,
+    //         pass: process.env.EMAIL_PASS,
+    //     },
+
+    // });
     const transporter = nodemailer.createTransport({
-        service: 'gmail', // For Gmail in production
-        
-        // host: "smtp.mailtrap.io",   // Mailtrap SMTP host
-        // port: 2525, // For Mailtrap
-        
+        host: 'smtp.gmail.com',
+        port: 465,
+        secure: true, // Use SSL
         auth: {
             user: process.env.EMAIL_USER,
             pass: process.env.EMAIL_PASS,
         },
-
+        tls: {
+            // This is crucial for cloud servers to prevent SSL routing hangups
+            rejectUnauthorized: false 
+        },
+        // Force Node to wait longer for the cloud network handshake
+        connectionTimeout: 20000, 
+        socketTimeout: 20000,
     });
 
     const mailOptions = {
