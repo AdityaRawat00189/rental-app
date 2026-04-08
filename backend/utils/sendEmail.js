3e918e71dd35e7fb5f9ff2d275238932652248c4
Aditya Rawat <rawataditya415@gmail.com>
2026-04-08 11:57:01 +0530

    Transporter Changed, trying to fix the pblm of nodemailer

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -15,6 +15,22 @@ const sendEmail = async (options) => {
     //     },
 
     // });
+    // const transporter = nodemailer.createTransport({
+    //     host: 'smtp.gmail.com',
+    //     port: 465,
+    //     secure: true, // Use SSL
+    //     auth: {
+    //         user: process.env.EMAIL_USER,
+    //         pass: process.env.EMAIL_PASS,
+    //     },
+    //     tls: {
+    //         // This is crucial for cloud servers to prevent SSL routing hangups
+    //         rejectUnauthorized: false 
+    //     },
+    //     // Force Node to wait longer for the cloud network handshake
+    //     connectionTimeout: 20000, 
+    //     socketTimeout: 20000,
+    // });
     const transporter = nodemailer.createTransport({
         host: 'smtp.gmail.com',
         port: 465,
@@ -30,6 +46,9 @@ const sendEmail = async (options) => {
         // Force Node to wait longer for the cloud network handshake
         connectionTimeout: 20000, 
         socketTimeout: 20000,
+        
+        // 👉 ADD THIS EXACT LINE:
+        family: 4 
     });
 
     const mailOptions = {
